d62f7747e7e72e7aee7f281c6a8cabc6882f701e
IngaMoss <83215456+IngaMoss@users.noreply.github.com>
2021-05-07 11:43:43 +0200

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    let days = ["Synday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +23,8 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "6a4eedc94ba63bc335cf63285a0c725f";
